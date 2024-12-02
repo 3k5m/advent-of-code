87d063597a9c49ed443eeb83dcf4824c75041572
3k5m <96869224+3k5m@users.noreply.github.com>
2024-12-03 00:55:00 +0900

    day 1

---
+++ solutions/day1-01.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int list1[10005], list2[10005];
+
+void solve(){
+    int sum = 0;
+    int i=0; 
+    while(true){
+        string s;
+        getline(cin, s);
+        if(s.empty()) break; 
+
+        int input1 = 0, input2 = 0;
+        
+        int digit = 4;
+        bool i2 = false;
+        for(char c: s){
+            if(c == ' ') continue;
+            else{
+                if(digit < 0){
+                    digit = 4;
+                    i2=true;
+                }
+                if(i2) input2 += pow(10, digit) * (c - '0');
+                else input1 += pow(10, digit) * (c - '0');
+                digit --;
+            }
+        }
+
+        list1[i] = input1;
+        list2[i] = input2;
+        i++;
+    }
+    sort(list1, list1 + i);
+    sort(list2, list2 + i);
+
+    for(int j=0;j<=i;j++){
+        sum += abs(list1[j] - list2[j]);
+    }
+    cout<<sum;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
