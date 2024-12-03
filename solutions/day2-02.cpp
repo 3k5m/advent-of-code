bcffdab246310a0014a53e24d16cdf03e8d1b5d1
3k5m <96869224+3k5m@users.noreply.github.com>
2024-12-03 20:03:30 +0900

    day 2

--- solutions/day2-02.cpp
+++ solutions/day2-02.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define IS_DEBUGGING false
+
 //not finished yet
 //TODO: implement isSafe, change logic to turn string into array of ints
 //attempt isSafe with each variation (remove one report at a time)
@@ -8,53 +10,51 @@ using namespace std;
 
 //pass in reports and length to loop through
 bool isSafe(int reports[105], int l){
-    int curNum, prevNum;
-    int errors = 0;
-    bool incFlag = false, decFlag = false, firstNum = true;
+    int curNum = -1, prevNum;
+    bool incFlag = false, decFlag = false;
     bool unsafe = false;
-    int digit = 1, nextNum = 0;
-    string num = "";
     
-    for(char c: ){
-        if(c == ' '){
-            nextNum = stoi(num);
-            num = "";
-            if(firstNum){
-                curNum = nextNum;
-                firstNum = false;
-            }else{
-                prevNum = curNum;
-                curNum = nextNum;
-                if(curNum > prevNum) incFlag = true;
-                if(curNum < prevNum) decFlag = true;
-                if(curNum == prevNum) {
-                    unsafe = true;
-                    //cout<<curNum<<" is equal to "<<prevNum<<", report failed.\n";
-                    errors ++;
-                }
+    if(IS_DEBUGGING) cout<<"looping through report with length = "<<l<<"\n";
+    if(IS_DEBUGGING) cout<<"Report output: ";
+    if(IS_DEBUGGING) for(int i=0;i<l;i++) cout<<reports[i]<<" ";
+    if(IS_DEBUGGING) cout<<"\n";
+    for(int i = 0; i < l; i++){
 
-                if(abs(curNum - prevNum) > 3){
-                    unsafe = true;
-                    //cout<<curNum<<" and "<<prevNum<<" abs difference > 3, report failed.\n";
-                    errors ++;
-                }
+        if(reports[i] == -1){
+            if(IS_DEBUGGING) cout<<"skipped report value -1 at index "<<i<<"\n";
+            continue;
+        }
 
-                if(incFlag && decFlag){
-                    unsafe = true;
-                    //cout<<"  both incFlag & decFlag were set, report failed.\n";
-                    break;
-                }
-            }
-        }else{
-            num += c;
+        if(curNum == -1) {
+            curNum = reports[i];
+            continue;
+        }
+
+        prevNum = curNum;
+        curNum = reports[i];
+        if(curNum > prevNum) incFlag = true;
+        if(curNum < prevNum) decFlag = true;
+        if(curNum == prevNum) {
+            unsafe = true;
+            if(IS_DEBUGGING) cout<<curNum<<" is equal to "<<prevNum<<", report failed.\n";
+        }
+
+        if(abs(curNum - prevNum) > 3){
+            unsafe = true;
+            if(IS_DEBUGGING) cout<<curNum<<" and "<<prevNum<<" abs difference > 3, report failed.\n";
+        }
+
+        if(incFlag && decFlag){
+            unsafe = true;
+            if(IS_DEBUGGING) cout<<"  both incFlag & decFlag were set, report failed.\n";
+            break;
         }
     }
-    return unsafe;
+    return !unsafe;
 }
 
 void solve(){
-    int sum = 0;
-    int i=0; 
+    int sum = 0, guaranteedsum = 0;
     while(true){
         string s;
         getline(cin, s);
@@ -63,54 +63,39 @@ void solve(){
         //adding a space at the end to detect last number
         //hacky fix lol
         s += " ";
-        
-        int curNum, prevNum;
-        int errors = 0;
-        bool incFlag = false, decFlag = false, firstNum = true;
-        bool unsafe = false;
-        int digit = 1, nextNum = 0;
 
         int reportsToCheck[105];
         string num = "";
-
-        for(char c: s){
-            if(c == ' '){
-                nextNum = stoi(num);
+        int j = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == ' '){
+                reportsToCheck[j] = stoi(num);
                 num = "";
-                if(firstNum){
-                    curNum = nextNum;
-                    firstNum = false;
-                }else{
-                    prevNum = curNum;
-                    curNum = nextNum;
-                    if(curNum > prevNum) incFlag = true;
-                    if(curNum < prevNum) decFlag = true;
-                    if(curNum == prevNum) {
-                        unsafe = true;
-                        //cout<<curNum<<" is equal to "<<prevNum<<", report failed.\n";
-                        errors ++;
-                    }
-
-                    if(abs(curNum - prevNum) > 3){
-                        unsafe = true;
-                        //cout<<curNum<<" and "<<prevNum<<" abs difference > 3, report failed.\n";
-                        errors ++;
-                    }
-
-                    if(incFlag && decFlag){
-                        unsafe = true;
-                        //cout<<"  both incFlag & decFlag were set, report failed.\n";
-                        break;
-                    }
-                }
+                j++;
             }else{
-                num += c;
+                num += s[i];
             }
         }
+        bool safe = isSafe(reportsToCheck, j);
 
-        if(!unsafe) sum++;
+        if(!safe){
+            for(int i = 0; i < j; i++){
+                int tempReports[105];
+                copy(reportsToCheck, reportsToCheck + j, tempReports);
+                tempReports[i] = -1;
+                bool tempS = isSafe(tempReports, j);
+                if(tempS) {
+                    safe = true;
+                    break;
+                }
+            }
+        }else {
+            guaranteedsum++;
+        }
+
+        if(safe) sum++;
     }
-    cout<<sum;
+    cout<<sum <<" with dampen, "<<guaranteedsum<<" without.\n";
 }
 
 int main() {
