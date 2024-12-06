2570976a5b8165c43879bcdd4bbc55c2f3e6c3d8
3k5m <96869224+3k5m@users.noreply.github.com>
2024-12-06 17:46:59 +0900

    day 5 part 2

--- solutions/day5-02.cpp
+++ solutions/day5-02.cpp
@@ -1,34 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define IS_DEBUGGING true
+#define IS_DEBUGGING false
 
-// failed to solve this part
-// work in progress code for future reference
-// need to learn more about sorting
-// (still don't know how to implement quicksort and just use c++ built-in sort)
+// was going to give up on this part but I saw this reddit comment saying to just swap when a rule is violated
+// https://www.reddit.com/r/adventofcode/comments/1h71eyz/comment/m0m81aj/
+// thank you u/Enemiend
 
 bool mustBeBefore[105][105] = {0};
 int pages[105] = {0};
-bool pageCorrect[105] = {0};
 
 bool checkUpdate(int i){
     bool correct = true;
     for(int j=0; j < i; j++){
-        if(pageCorrect[j]) continue;
-        bool thisPageC = true;
         for(int k=0; k < i; k++){
             int n1 = pages[j], n2 = pages[k];
             if(mustBeBefore[n1][n2] && j > k){
                 correct = false;
-                thisPageC = false;
+                if(IS_DEBUGGING) cout<<"Swapping "<<pages[j]<<" and "<<pages[k]<<"\n";
+                //swapping should hopefully resolve all rule breaks
+                int temp = pages[k];
+                pages[k] = pages[j];
+                pages[j] = temp;
+
             }
         }
-        if(thisPageC){
-            pageCorrect[j] = true;
-            if(IS_DEBUGGING) cout<<pages[j]<<" has been shown as correct.\n";
-        }
-
     }
     return correct;
 }
@@ -78,7 +74,7 @@ void solve(){
     //then the test cases
     while(true){
         string num = "";
-        for(int z=0;z<105;z++) {pages[z] = 0; pageCorrect[z] = false;}
+        for(int z=0;z<105;z++) {pages[z] = 0;}
         
         int i=0;
 
@@ -105,24 +101,15 @@ void solve(){
         if(checkUpdate(i)){
             if(IS_DEBUGGING) cout<<"Rule " + inputLine + " was already correct, no need to add its middle.\n";
         }else{
-            //using randomness to swap positions of two incorrect tests
-            srand((unsigned)time(0)); 
-            int steps = 0;
-
-            while(!checkUpdate(i)){
-                steps ++; if(steps > 2000) break;
-                int r = (rand()%(i-1));
-                int r2 = (rand()%(i-1));
-                if(!pageCorrect[r] && !pageCorrect[r2]){
-                    if(IS_DEBUGGING) cout<<"Swapping "<<pages[r]<<" and "<<pages[r2]<<"\n";
-
-                    int temp = pages[r];
-                    pages[r] = pages[r2];
-                    pages[r2] = temp;
+            if(IS_DEBUGGING){
+                cout<<"Original Rule " + inputLine<<"\n";
+                cout<<"Corrected Rule: ";
+                for(auto x : pages){
+                    if(x == 0) break;
+                    cout<<x<<", ";
                 }
+                cout<<"\nCorrected middle: "<<pages[(i)/2]<<"\n";
             }
-
-            if(IS_DEBUGGING) cout<<"Rule " + inputLine + " corrected middle: "<<pages[(i)/2]<<"\n";
             sum += pages[(i)/2];
         }
     }
