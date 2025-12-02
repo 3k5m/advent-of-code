a07601e9181daba51785b9d95dcdec3d1df0d51f
3k5m <96869224+3k5m@users.noreply.github.com>
2025-12-02 18:27:04 -0500

    removed unnecessary file

---
+++ solutions/2025/day2-01.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXROT = 99;
+
+void solve(){
+    int flag = 0;
+    string input;
+    int curROT = 50;
+    while(true){
+        getline(cin, input);
+        if(input.empty()){
+            break;
+        }
+        if(input[0] == 'L'){
+            input.erase(0, 1);
+            int steps = stoi(input);
+            while(steps--){
+                curROT -= 1;
+                if(curROT == -1){
+                    curROT = 99;
+                }
+            }
+        }else if (input[0] == 'R'){
+            input.erase(0, 1);
+            int steps = stoi(input);
+            while(steps--){
+                curROT += 1;
+                if(curROT == 100){
+                    curROT = 0;
+                }
+            }
+        }
+
+        if(curROT == 0){
+            flag ++;
+        }
+    }
+    cout<<flag<<"\n";
+    system("pause");
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
